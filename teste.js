e025b2bec3d3e296ccf5cca31ae8178492734aac
thaysllan <103584151+thaysllan@users.noreply.github.com>
2022-06-19 21:42:50 -0400

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,7 @@
+let nome = "Thaysllan"
+let sobrenome = "Fonseca"
+let anoNascimento = 2002
+let cidade = "Porto Velho" 
+var anoAtual = new Date().getFullYear();
+console.log ( 'Meu nome é ' + nome  + sobrenome +  '. Eu tenho ' + (anoAtual - anoNascimento) + ' anos e resido atualmente em ' + cidade )
+
